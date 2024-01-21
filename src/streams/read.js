ceeea04365bce78f675a8566ef800a3d4941ec33
Artem Mironchik <tawerka.power@gmail.com>
2024-01-21 22:53:21 +0300

    feat: add solutions for streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { join } from 'node:path';
+import { createReadStream } from 'node:fs';
+
+import { getDirname } from '../utils/index.js';
+
+const __dirname = getDirname(import.meta.url);
+const pathToFile = join(__dirname, 'files', 'fileToRead.txt');
+
+const stream = createReadStream(pathToFile);
+
 const read = async () => {
-    // Write your code here 
+    stream.pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { join } from 'node:path';
+import { createWriteStream } from 'node:fs';
+
+import { getDirname } from '../utils/index.js';
+
+const __dirname = getDirname(import.meta.url);
+const pathToFile = join(__dirname, 'files', 'fileToWrite.txt');
+
+const stream = createWriteStream(pathToFile);
+
 const write = async () => {
-    // Write your code here 
+    process.stdin.pipe(stream);
 };
 
 await write();
